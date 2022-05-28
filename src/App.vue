0d005f85226fdcc94bf52e0749de4a261e9748b8
unknown <calikridvan37@outlook.com.tr>
2022-05-28 15:39:42 +0300

    loading

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import MainVue from './Main.vue';
+</script>
+
+<template>
+  <section class="a">
+    <div class="title">
+      <h1>Malenia Blade Of Miqulla</h1>
+      <iframe scrolling="no" src="./program/index.html"></iframe>
+    </div>
+  </section>
+  <section>
+    <MainVue></MainVue>
+  </section>
+</template>
+
+<style>
+#app {
+  scroll-snap-type: y mandatory;
+  margin: 0 auto;
+  height: 100vh;
+  font-weight: normal;
+  overflow-y: scroll;
+}
+
+#app::-webkit-scrollbar {
+  display: none;
+}
+
+section {
+  scroll-snap-align: start;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+}
+
+.title h1 {
+  letter-spacing: 0.5em;
+  font-size: 2rem;
+  color: white;
+  z-index: 1000;
+  text-align: center;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  top: 50%;
+  position: absolute;
+}
+
+iframe {
+  box-shadow: 10px 10px 8px 10px #080606;
+  border: none;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/Main.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <div class="main">
+        <p>Malenia Miqulla'nın Kılıcı...<br>
+            Radogan ve Kraliçe Marikanın ikiz çoçuklarından biri olan Malenia, kardeşi gibi hastalıklı doğmuştu. İçinde
+
+            korkunç bir zehir barındırmaktaydı, ama onun son derece kutsal olması
+            Greater Will'in mirası için uygun bir aday yapıyordu.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    height: 100%;
+}
+
+
+
+.main p {
+    font-size: 2em;
+    text-indent: 50px;
+    text-align: justify;
+    letter-spacing: 3px;
+}
+
+@media (max-width:854px) {
+    .main p {
+        font-size: 1.2em;
+
+    }
+}
+</style>
